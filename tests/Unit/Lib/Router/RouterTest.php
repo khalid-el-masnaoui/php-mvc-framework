895f904a9747754bee6d8dd9be3c05768b6da289
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2024-12-20 23:45:52 +0100

    setup dev branch and configure routes branch

---
+++ tests/Unit/Lib/Router/RouterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Lib\Router;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class RouterTest extends TestCase
+{
+    #[Test]
+    public function itWorks(): void
+    {
+        $this->assertTrue(3 === 3);
+    }
+}
