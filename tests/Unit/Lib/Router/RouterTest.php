4670593de060805bcb613747e3068ae16d5ca4db
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2024-12-20 23:48:27 +0100

    setting up the tests with php-unit

--- tests/Unit/Lib/Router/RouterTest.php
+++ tests/Unit/Lib/Router/RouterTest.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Lib\Router;
 
+use App\Lib\Router\Router;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\CoversClass;
 
+#[CoversClass(Router::class)]
 class RouterTest extends TestCase
 {
     #[Test]
